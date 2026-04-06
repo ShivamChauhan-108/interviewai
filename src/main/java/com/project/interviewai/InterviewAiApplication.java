5ec19350413a0a31cc0fc36d05f0d7ae2dce667b
prince chauhan <akhil.kumar@cyfuture.com>
2026-04-06 12:13:40 +0530

    chore: setup initial Spring Boot project with DB configs

---
+++ src/main/java/com/project/interviewai/InterviewAiApplication.java
@@ -0,0 +1,13 @@
+package com.project.interviewai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class interviewaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(interviewaiApplication.class, args);
+	}
+
+}
